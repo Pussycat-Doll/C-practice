afed3a0f1d4586928caadb86232c5177ec94bce1
柳阿九 <49825031+Pussycat-Doll@users.noreply.github.com>
2019-10-24 23:02:16 +0800

    Add files via upload

---
+++ 3. 输出一个整数的每一位/3. 输出一个整数的每一位/输出一个整数的每一位.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//3. 输出一个整数的每一位
+#include<stdio.h>
+#include<stdlib.h>
+void print(int num)
+{
+	if (num > 9)
+	{
+		print(num / 10);
+	}
+	printf("%d ", num % 10);
+}
+int main()
+{
+	int num = 0;
+	scanf("%d", &num);
+	print(num);
+	system("pause");
+	return 0;
+}
