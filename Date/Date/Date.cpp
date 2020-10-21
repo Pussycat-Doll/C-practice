93d56691ab3099a1fb04c14ed77fead336a8a00d
Pussycat-Doll <2633974012@qq.com>
2020-10-21 22:13:51 +0800

    2020/10/21

--- Date/Date/Date.cpp
+++ Date/Date/Date.cpp
@@ -4,11 +4,24 @@ using namespace std;
 class Date
 {
 public:
-	Date(int year = 1900, int month = 1, int day = 1)//构造函数
-		:_year(year)
-		,_month(month)
-		,_day(day)
-	{}
+	//Date(int year = 1900, int month = 1, int day = 1)//构造函数
+	//	:_year(year)
+	//	,_month(month)
+	//	,_day(day)
+	//{}
+	Date(int year = 0, int month = 1, int day = 1)//构造函数
+	{
+		if (year >= 0 && month >= 0 && month <= 12 && day >= 1 && day <= getmonthday(year, month))
+		{
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+		else
+		{
+			cout << "该日期为非法日期"<<endl;
+		}
+	}
 	Date(const Date& d)//拷贝构造函数
 	{
 		_year = d._year;
@@ -24,13 +37,14 @@ public:
 			_day = d._day;
 		}
 	}
-	bool islooneryear()
+	bool islooneryear(int year)
 	{
-		return ((_year % 4 == 0) || (_year % 400 == 0 && _year % 4 != 0));
+		return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
+		//四年一润且百年不润或者400年一润
 	}
-	int getmonthday()
+	int getmonthday(int year, int month)
 	{
-		switch (_month)
+		switch (month)
 		{
 		case 1:
 		case 3:
@@ -48,12 +62,10 @@ public:
 			return 30;
 			break;
 		case 2:
-			if (islooneryear())
+			if (islooneryear(year))
 				return 29;
 			else
 				return 28;
-			break;
-		default:
 			break;
 		}
 	}
@@ -61,17 +73,32 @@ public:
 	{
 		Date d1(*this);
 		d1._day += days;
-		while (d1._day > d1.getmonthday())//判断月份是否需要进位
+		while (d1._day > d1.getmonthday(d1._year,d1._month))//判断月份是否需要进位
 		{
-			d1._day -= getmonthday();
+			d1._day -= getmonthday(d1._year,d1._month);
 			++d1._month;
-		}
-		while (d1._month > 12)
-		{
-			d1._month -= 12;
-			++d1._year;
+			if (d1._month == 13)
+			{
+				d1._month = 1;
+				++d1._year;
+			}
 		}
 		return d1;
+	}
+	Date& operator+=(int days)
+	{
+		_day += days;
+		while (_day > getmonthday(_year, _month))//判断月份是否需要进位
+		{
+			_day -= getmonthday(_year, _month);
+			++_month;
+			if (_month == 13)
+			{
+				_month = 1;
+				++_year;
+			}
+		}
+		return *this;
 	}
 	Date operator-(int days)
 	{
@@ -84,7 +111,7 @@ public:
 		while (d1._day <= days)
 		{
 			--d1._month;
-			d1._day = d1._day + d1.getmonthday() - days;
+			d1._day = d1._day + d1.getmonthday(_year,_month) - days;
 		}
 	}
 	int operator-(const Date& d);
@@ -92,12 +119,38 @@ public:
 	Date operator++(int);//后置++
 	Date& operator--();//前置--
 	Date operator--(int);//后置--
-	bool operator>(const Date& d)const;
-	bool operator==(const Date& d)const;
-	bool operator<(const Date& d)const;
-	bool operator!=(const Date& d)const;
-	bool operator<=(const Date& d)const;
-	bool operator>=(const Date& d)const;
+	bool operator>(const Date& d)const
+	{
+		return (!(*this <= d));
+	}
+	bool operator==(const Date& d)const
+	{
+		return _year == d._year && _month == d._month && _day == d._day;
+	}
+	//高内聚，低耦合
+	bool operator<(const Date& d)const
+	{
+		if (_year < d._year)
+			return true;
+		else if (_year == d._year && _month < d._month)
+			return true;
+		else if (_year == d._year && _month == d._month && _day < d._day)
+			return true;
+		else
+			return false;
+	}
+	bool operator!=(const Date& d)const
+	{
+		return (!(*this == d));
+	}
+	bool operator<=(const Date& d)const
+	{
+		return (*this < d || *this == d);
+	}
+	bool operator>=(const Date& d)const
+	{
+		return (!(*this < d));
+	}
 	void print()
 	{
 		cout << _year << "-" << _month << "-" << _day << endl;
@@ -110,9 +163,8 @@ private:
 int main()
 {
 	Date d1(2000, 2, 3);
-	cout << d1.getmonthday() << endl;
 
-	Date d2 = d1.operator+(27);
-	d2.print();
+
+
 	return 0;
 }
